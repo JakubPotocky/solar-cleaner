28781e790d9b34e404605c1d70c81afb14b2b4ae
Jakub Potocky <jakubpatoky@gmail.com>
2025-12-06 14:54:53 +0100

    HAHA PLEASE WOORK DONT BURN

--- raspi/plswork.py
+++ raspi/plswork.py
@@ -1,21 +1,21 @@
 import RPi.GPIO as GPIO
 import time
 
-PWM_PIN = 18
-DIR_PIN = 23
-BRAKE_PIN = 24
+PWM_PIN = 13
+DIR_PIN = 26
+#BRAKE_PIN = 24
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(PWM_PIN, GPIO.OUT)
 GPIO.setup(DIR_PIN, GPIO.OUT)
-GPIO.setup(BRAKE_PIN, GPIO.OUT)
+#GPIO.setup(BRAKE_PIN, GPIO.OUT)
 
 pwm = GPIO.PWM(PWM_PIN, 20000)  # 20 kHz
 pwm.start(0)
 
 # Forward direction
 GPIO.output(DIR_PIN, GPIO.HIGH)
-GPIO.output(BRAKE_PIN, GPIO.LOW)
+#GPIO.output(BRAKE_PIN, GPIO.LOW)
 
 # Drive motor at 50% speed
 pwm.ChangeDutyCycle(50)
