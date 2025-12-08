a84ea48aad60dc8779a988a78a0ea83d2567b173
Jakub Potocky <jakubpatoky@gmail.com>
2025-12-08 17:17:07 +0100

    plss

--- raspi/plswork2motors.py
+++ raspi/plswork2motors.py
@@ -3,23 +3,31 @@ import time
 import signal
 import sys
 
-# BCM numbers (NOT physical pins)
-PWM_PIN_A = 13    # physical 33
-DIR_PIN_A = 26    # physical 37
+# Motor A – BCM numbers
+PWM_PIN_A  = 13   # physical 33
+DIR_PIN_A  = 26   # physical 37
 DIR_PIN2_A = 16   # physical 36
 
-PWM_PIN_B = 12    # physical 33
-DIR_PIN_B = 23    # physical 37
-DIR_PIN2_B = 14   # physical 36
+# Motor B – BCM numbers
+PWM_PIN_B  = 12   # physical 32
+DIR_PIN_B  = 23   # physical 16
+DIR_PIN2_B = 14   # physical 8
 
-FREQ = 20000    # 20 kHz
+FREQ = 20000  # 20 kHz
 
-# Graceful exit on Ctrl+C
-def cleanup(signum, frame):
-    print("\nStopping motor...")
+# Connect to pigpio daemon
+pi = pigpio.pi()
+if not pi.connected:
+    print("Failed to connect to pigpio daemon")
+    sys.exit(1)
+
+def cleanup(signum=None, frame=None):
+    print("\nStopping motors...")
+    # Motor A
     pi.set_PWM_dutycycle(PWM_PIN_A, 0)
     pi.write(DIR_PIN_A, 0)
     pi.write(DIR_PIN2_A, 0)
+    # Motor B
     pi.set_PWM_dutycycle(PWM_PIN_B, 0)
     pi.write(DIR_PIN_B, 0)
     pi.write(DIR_PIN2_B, 0)
@@ -28,26 +36,15 @@ def cleanup(signum, frame):
 
 signal.signal(signal.SIGINT, cleanup)
 
-# Connect to pigpio daemon
-pi = pigpio.pi()
-if not pi.connected:
-    print("Failed to connect to pigpio daemon")
-    sys.exit(1)
-
 # Set pin modes
-pi.set_mode(PWM_PIN_A, pigpio.OUTPUT)
-pi.set_mode(DIR_PIN_A, pigpio.OUTPUT)
-pi.set_mode(DIR_PIN2_A, pigpio.OUTPUT)
-
-pi.set_mode(PWM_PIN_B, pigpio.OUTPUT)
-pi.set_mode(DIR_PIN_B, pigpio.OUTPUT)
-pi.set_mode(DIR_PIN2_B, pigpio.OUTPUT)
+for pin in [PWM_PIN_A, DIR_PIN_A, DIR_PIN2_A, PWM_PIN_B, DIR_PIN_B, DIR_PIN2_B]:
+    pi.set_mode(pin, pigpio.OUTPUT)
 
 # Set PWM frequency
 pi.set_PWM_frequency(PWM_PIN_A, FREQ)
 pi.set_PWM_frequency(PWM_PIN_B, FREQ)
 
-# Set direction (example: forward)
+# Set direction (forward on both motors)
 pi.write(DIR_PIN_A, 1)
 pi.write(DIR_PIN2_A, 0)
 
@@ -58,8 +55,11 @@ pi.write(DIR_PIN2_B, 0)
 pi.set_PWM_dutycycle(PWM_PIN_A, 255)
 pi.set_PWM_dutycycle(PWM_PIN_B, 255)
 
-print("Motor running at full speed. Press Ctrl + C to stop.")
+print("Motors running at full speed. Press Ctrl + C to stop.")
 
-# Keep running forever
-while True:
-    time.sleep(1)
+try:
+    while True:
+        time.sleep(1)
+except Exception as e:
+    print("Error:", e)
+    cleanup()
